b1206cc523b0d2c3f7939081be3c59712b137d5f
AbdulQuduss <oladosuqudus@gmail.com>
2023-08-10 19:05:30 -0700

    low level programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
-	/* This program prints a sentence */
+/** 
+*main-This program prints "with proper grammar, but the outcome is a piece of art,"
+*return: 0(success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }

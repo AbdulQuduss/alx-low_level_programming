b07e21b707282ab8e6456116e4615dbe65d3be5f
AbdulQuduss <oladosuqudus@gmail.com>
2023-08-10 18:47:44 -0700

    dfd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main- This program prints "Programming is like building a multilingual puzzle"
+ *main:This program prints "Programming is like building a multilingual puzzle"
  *Return: 0(success)
  */
 int main(void)

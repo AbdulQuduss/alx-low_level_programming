cc27958b03a8ed84f1d02c38c145aecb2d7c0fd1
AbdulQuduss <oladosuqudus@gmail.com>
2023-08-14 19:20:52 -0700

    variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,14 @@
 /*
  * This program prints lowercase alphabets
  */
-int main(void) 
+int main(void)
 {
 	char lowercase = 'a';
 	int i;
-	
 	for (i = 0; i < 26; i++)
 	{
 		putchar(lowercase + i);
 	}
-
 	putchar('\n');
-	
 	return (0);
 }

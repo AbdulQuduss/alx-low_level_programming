794041664546dbad6343eafa686aba15052278d4
AbdulQuduss <oladosuqudus@gmail.com>
2023-08-15 13:17:05 -0700

    functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,17 @@
-#include <stdio.h>
-/**
- * print_alphabet - This function prints lowercase
- */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 void print_alphabet(void)
 {
 	char lowercase = 'a';
 	int i;
 
-	for (i = 0; i < 26; i++) 
+	for (i = 0; i < 26; i++)
 	{
 	putchar(lowercase);
 	lowercase++;
 	}
-
 	putchar('\n');
 }

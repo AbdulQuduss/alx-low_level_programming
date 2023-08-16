01529bb39c0de974d7013852e7619fa0ed4a828b
AbdulQuduss <oladosuqudus@gmail.com>
2023-08-16 06:39:31 -0700

    latest

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,7 @@ void print_jack_bauer_minutes(void)
 	{
 	int minute;
 	
-	for (int minute = 0; minute < 60; minute++) 
+	for (minute = 0; minute < 60; minute++) 
 	{
 	_putchar('0' + hour / 10);
 	_putchar('0' + hour % 10);

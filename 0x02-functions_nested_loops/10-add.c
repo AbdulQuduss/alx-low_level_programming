1025d2d8573d13ce0cd7bf7a229eb419e7dcb878
AbdulQuduss <oladosuqudus@gmail.com>
2023-08-16 06:18:58 -0700

    loud

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - This functon ads two integers
+ * @a : the first input
+ * @b : The second input
+ * Return: result after addition
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+void print_jack_bauer_minutes(void) 
+{
+	int hour;
+
+	for (hour = 0; hour < 24; hour++) 
+	{
+	int minute;
+	
+	for (int minute = 0; minute < 60; minute++) 
+	{
+	_putchar('0' + hour / 10);
+	_putchar('0' + hour % 10);
+	_putchar(':');
+	_putchar('0' + minute / 10);
+	_putchar('0' + minute % 10);
+	_putchar('\n');
+	}
+	}
+}

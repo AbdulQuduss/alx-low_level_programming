ffff03870e15d9fe43c6fe14ccb944cc9073a8ab
AbdulQuduss <oladosuqudus@gmail.com>
2023-08-16 03:13:28 -0700

    aaww

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,15 +9,9 @@
  * Return: On success, returns c.
  * On error, returns -1 and sets errno
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
-/**
- * print_alphabet - This function displays alphabets
- * Return: void
- */
+int _putchar(char c);
+void print_alphabet_x10(void);
 void print_alphabet(void);
 
 #endif

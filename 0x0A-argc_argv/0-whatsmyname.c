7dcef08a531e9686c157b6513ad0c7425b344c2e
AbdulQuduss <oladosuqudus@gmail.com>
2023-09-04 07:55:16 -0700

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * nargs- prints the name of the program
+ * main- prints the name of the program
  * @argc: argument count
  * @argv: argument array
  * Return: always 0
  */
-int nargs(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);

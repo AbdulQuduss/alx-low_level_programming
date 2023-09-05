a1173d4ef116d5e58349f9a31d36e5632d10b31a
memphis <oladosuqudus@gmail.com>
2023-09-05 23:40:59 +0100

    ahah

--- 0x09-static_libraries/3-islower.c
+++
@@ -1,9 +0,0 @@
-/**
- * _islower - This function checks if @c is a lower character
- * @c: input character
- * Return: 1 if successful but 0 if not
- */
-int _islower(int c)
-{
-	return (c >= 'a' && c <= 'z');
-}

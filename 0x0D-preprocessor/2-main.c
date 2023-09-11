5b4761ecb38cf487eebbbec09a57ede4b102084d
memphis <oladosuqudus@gmail.com>
2023-09-11 16:04:17 +0100

    dream

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- This program prints the name of the file it was compiled from
+ * Return: o
+ */
+
+char main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
